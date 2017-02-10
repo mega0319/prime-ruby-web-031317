404e4ec0e6f5aa1b4612f598c38ed9954dc9a1c2
Naz Khan <nazmuskhan@yahoo.com>
2017-02-10 08:35:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+  number_array = (2...number).to_a
+  number_tray = []
+  number_array.each do |x|
+    if number % x == 0
+      number_tray.push(x)
+    end
+  end
+  if number <= 1
+    false
+  elsif number % 1 == 0 && number % number == 0 && number_tray.length == 0
+    true
+  else
+    false
+  end
+end
